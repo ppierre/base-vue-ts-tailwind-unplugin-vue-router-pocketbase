d3b5bb60c8992658f54ead3c927a5a158234bccd
pierre pracht <pierre.pracht@gmail.com>
2024-01-27 20:04:26 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <h1>Test</h1>
+
+    <nav class="*:text-red-500 flex flex-col">
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>

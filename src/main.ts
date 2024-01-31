6f2383d4025329f92a98e63a2a0db17160c44e84
pierre pracht <pierre.pracht@gmail.com>
2024-01-31 12:39:12 +0100

    utilise vite-plugin-pages plus simple, désolé

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,15 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import { createRouter, createWebHistory } from 'vue-router/auto'
+import { createRouter, createWebHistory } from 'vue-router'
+import routes from '~pages'
 
 const app = createApp(App)
 
 app.use(
   createRouter({
-    history: createWebHistory()
+    history: createWebHistory(),
+    routes
   })
 )
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { fileURLToPath, URL } from 'node:url'
-import VueRouter from 'unplugin-vue-router/vite'
+import Pages from 'vite-plugin-pages'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [VueRouter({}), vue()],
+  plugins: [Pages({}), vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))

e0757217db1c8df80ed32226f9567520d6c7934c
pierre pracht <pierre.pracht@gmail.com>
2024-01-31 13:54:49 +0100

    Revert "utilise vite-plugin-pages plus simple, désolé"
    This reverts commit 6f2383d4025329f92a98e63a2a0db17160c44e84.

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,13 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import { createRouter, createWebHistory } from 'vue-router'
-import routes from '~pages'
+import { createRouter, createWebHistory } from 'vue-router/auto'
 
 const app = createApp(App)
 
 app.use(
   createRouter({
-    history: createWebHistory(),
-    routes
+    history: createWebHistory()
   })
 )
 

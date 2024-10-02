da7498fb3e4cd50d6d66f9d79f6ab2813e3e7277
pierre pracht <pierre.pracht@gmail.com>
2024-10-02 13:37:04 +0200

    m-a-j

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import { createRouter, createWebHistory } from 'vue-router/auto'
+import { routes } from 'vue-router/auto-routes'
 
 const app = createApp(App)
 
 app.use(
   createRouter({
-    history: createWebHistory()
+    history: createWebHistory(),
+    routes
   })
 )
 

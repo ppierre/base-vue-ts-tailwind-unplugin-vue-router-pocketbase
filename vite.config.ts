9fbd9c35524e1c367dd86aba3b094802142595a1
pierre pracht <pierre.pracht@gmail.com>
2024-10-09 15:14:54 +0200

    add dev tool

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { fileURLToPath, URL } from 'node:url'
 import VueRouter from 'unplugin-vue-router/vite'
+import VueDevTools from 'vite-plugin-vue-devtools'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [VueRouter({}), vue()],
+  plugins: [VueRouter({}), vue(), VueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
